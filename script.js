bb781682187b461d9d2cdbfc6bce0608832e8b42
Lucas Fagundes <49763895+lucasfagundesf@users.noreply.github.com>
2022-10-08 12:02:46 -0300

    Projeto não terminado

---
+++ script.js
@@ -0,0 +1,8 @@
+function click(){
+    var inf = document.querySelector('.inf')
+    if(inf.style.display == 'block'){
+        inf.style.display = 'none'
+    }else{
+        inf.style.display = 'block'
+    }
+}
